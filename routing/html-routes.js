bd96dc692ca537ba23e55f619d414c569aafc451
victorsc45 <victorlitzau@gmail.com>
2020-07-30 11:56:18 -0400

    notes html path working need functionality for taking notes now

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = function (app) {
-    app.get('/notes', function (req, res) {
-        res.sendFile(path.join(__dirname + '/notes.html'));
+    app.get('api/notes', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/notes.html'));
     });
+    app.get('api/*', function (app) {
+        res.sendFile(path.join(__dirname + '/../public/index.html'));
+    })
     app.use(function (req, res) {
-        res.sendFile(path.join(__dirname + '/index.html'));// check routing here!!!!
+        res.sendFile(path.join(__dirname + '/../public/index.html'));// check routing here!!!!
     });
 }
--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json())
 
 // Routes
 // =============================================================
+require('./routing/html-routes.js')(app);
 require('./routing/api-routes.js')(app);
-require('./routing/html-routes.js.js')(app);
 
 
 // Starts the server to begin listening
