2b18ff1c14b66f8d0abad9f94eb50ad16d5a06d6
victorsc45 <victorlitzau@gmail.com>
2020-08-04 14:15:26 -0400

    application deployed to Heroku documentation corrected

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 const dbDir = require('./db/db');
 
 // File route for notes html page of the view
+app.get("/", function (req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+});
+
 app.get('/notes', (req, res) => {
 
     res.sendFile(path.join(__dirname, "./public/notes.html"));
