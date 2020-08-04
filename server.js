a1fba5f8afc12db075e95da7acdbbec760a30401
victorsc45 <victorlitzau@gmail.com>
2020-08-04 10:56:56 -0400

    getting the view to work next

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ const dbDir = require('./db/db');
 app.get('/notes', function (req, res) {
 
 
-    response.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 
 })
 
 
 app.get('*', function (req, res) {
 
-    response.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Routes API
